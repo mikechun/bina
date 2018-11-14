f5214e7b4b7386641b8eb94e39b79f1f2e8a2df1
Michael Chun <mikechun@gmail.com>
2018-11-14 14:23:24 -0800

    Fix asset path.  Fix distorted logo

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ class Header extends React.Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light header">
           <a className="navbar-brand" href="#">
-            <img src="/assets/logo_black.png" width="30" height="30" alt="" />
+            <img src="assets/logo_black.png" width="30" alt="" />
           </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
