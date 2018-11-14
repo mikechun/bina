2ea4241100e80721e026bcbbdf42cdc0de4c517d
Michael Chun <mikechun@gmail.com>
2018-11-14 14:18:00 -0800

    Updates

---
+++ precache-manifest.c1681796fc6cbd498b35c9330d7caa83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63df57a6bfb7fb2b27ab",
+    "url": "/bina/static/js/runtime~main.63df57a6.js"
+  },
+  {
+    "revision": "2df2d71f391439b4e70c",
+    "url": "/bina/static/js/main.2df2d71f.chunk.js"
+  },
+  {
+    "revision": "d65d1c7272c80c4fe5b1",
+    "url": "/bina/static/js/1.d65d1c72.chunk.js"
+  },
+  {
+    "revision": "2df2d71f391439b4e70c",
+    "url": "/bina/static/css/main.20aa70b9.chunk.css"
+  },
+  {
+    "revision": "857dccc7b9f30068f292df6a41ed3981",
+    "url": "/bina/index.html"
+  }
+];
