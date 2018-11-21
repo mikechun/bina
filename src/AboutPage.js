ba98f8bcd3af3159335aae355ff4b1f4a491b455
Michael Chun <mikechun@gmail.com>
2018-11-21 00:40:13 -0800

    Fix AboutPage styles

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -6,52 +6,50 @@ import WorkPanel from './WorkPanel';
 import './AboutPage.scss';
 
 export default () => {
-  return <div>
-    <div className='row'>
-      <div className='myphoto col-md-12 col-lg-5'>
-        <img src='assets/about1.jpg' data-rjs="2" />
+  return <div className='aboutPage'>
+    <div className='profilePhoto col-12 col-md-5'>
+      <img src='assets/about1.jpg' data-rjs="2" />
+    </div>
+    <div className='statementSection col-12 col-md-7'>
+      <div className='statement'>
+        <img className='quote-l' src='assets/Quotation1.png' data-rjs='2'/>I'm not just a <span className='highlight'>problem-solving</span> designer. I can <span className='highlight'>creating</span> something that <span className='highlight'>inspires people</span> with new <span className='highlight'>experiences</span>.<img src='assets/Quotation2.png' className='quote-r' data-rjs='2'/>
       </div>
-      <div className='statementSection col-md-12 col-lg-7'>
-        <div className='statement'>
-          <h2>I'm not just a problem-solving designer. I can creating something that inspires people with new experience.</h2>
-        </div>
-        <div className='resume'>
-        <button type='button' class='btn'><h3>RESUME &gt;</h3></button>
-        </div>
+      <div className='resumeButton col-6'>
+        <button type='button' class='btn'>RESUME &gt;</button>
       </div>
     </div>
-    <div className='row bio'>
-      <div className='col-lg-6'>
-        <span className='title'><h2>EXPERIENCE</h2></span>
+    <div className='bio'>
+      <div className='col-12 col-lg-6'>
+        <span className='section'>EXPERIENCE</span>
         <div className='exp'>
-          <h4><span className='company'>Hyundai Card</span> Senior Product Designer</h4>
-          <h6>July 2014 - May 2017</h6>
-          <h5>Conducted Fintech product strategies, designed responsive websites and mobile app.</h5>
+          <div className='title'><span className='company'>Hyundai Card</span> Senior Product Designer</div>
+          <div className='period'>July 2014 - May 2017</div>
+          <div className='description'>Conducted Fintech product strategies, designed responsive websites and mobile app.</div>
         </div>
         <div className='exp'>
-          <h4><span className='company'>Korea Telecom</span> Senior Product Designer</h4>
-          <h6>April 2012 - June 2014</h6>
-          <h5>Designed a camera app and a music streaming app.</h5>
+          <div className='title'><span className='company'>Korea Telecom</span> Senior Product Designer</div>
+          <div className='period'>April 2012 - June 2014</div>
+          <div className='description'>Designed a camera app and a music streaming app.</div>
         </div>
         <div className='exp'>
-          <h4><span className='company'>SK Communications</span> Product Designer</h4>
-          <h6>January 2006 - December 2010</h6>
-          <h5>Developed the largest social nework service in South Korea. Designed photo editor</h5>
+          <div className='title'><span className='company'>SK Communications</span> Product Designer</div>
+          <div className='period'>January 2006 - December 2010</div>
+          <div className='description'>Developed the largest social nework service in South Korea. Designed photo editor</div>
         </div>
       </div>
-      <div className='col-lg-6'>
-        <span className='title'><h2>EDUCATION</h2></span>
+      <div className='col-12 col-lg-6'>
+        <span className='section'>EDUCATION</span>
         <div className='exp'>
-          <h4><span className='company'>Academy of Art University</span> San Francisco, CA</h4>
-          <h5>Master of Arts / Web Design & New Media (Fall 2018)</h5>
+          <div className='title'><span className='company'>Academy of Art University</span> San Francisco, CA</div>
+          <div className='description'>Master of Arts / Web Design & New Media (Fall 2018)</div>
         </div>
         <div className='exp'>
-          <h4><span className='company'>Hanyang University</span> Seoul, Korea</h4>
-          <h5>Bachelor of Fine Arts / Visual Design (Spring 2006)</h5>
+          <div className='title'><span className='company'>Hanyang University</span> Seoul, Korea</div>
+          <div className='description'>Bachelor of Fine Arts / Visual Design (Spring 2006)</div>
         </div>
-        <span className='title'><h2>SKILLS</h2></span>
+        <span className='section'>SKILLS</span>
         <div className='exp'>
-          <h5>Sketch, Flinto, Invision, Adobe XD / Illustrator / Photoshop / Indesign / After effect</h5>
+          <div className='description'>Sketch, Flinto, Invision, Adobe XD / Illustrator / Photoshop / Indesign / After effect</div>
         </div>
       </div>
     </div>
