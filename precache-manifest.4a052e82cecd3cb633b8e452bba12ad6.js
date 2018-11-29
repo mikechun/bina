1311b179b5826d3a49e8a54174b75f5e19415fb5
Michael Chun <mikechun@gmail.com>
2018-11-28 19:22:45 -0800

    Updates

---
+++ precache-manifest.4a052e82cecd3cb633b8e452bba12ad6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63df57a6bfb7fb2b27ab",
+    "url": "/bina/static/js/runtime~main.63df57a6.js"
+  },
+  {
+    "revision": "a14ce1d5cbc4e988993f",
+    "url": "/bina/static/js/main.a14ce1d5.chunk.js"
+  },
+  {
+    "revision": "5876d39a78c0d98f9485",
+    "url": "/bina/static/js/1.5876d39a.chunk.js"
+  },
+  {
+    "revision": "a14ce1d5cbc4e988993f",
+    "url": "/bina/static/css/main.e5b8b72f.chunk.css"
+  },
+  {
+    "revision": "d64dc5afc4186c88db2d907369600cd5",
+    "url": "/bina/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bina/precache-manifest.7f9a195ad44d7fe51a8f926ebcc6882a.js"
+  "/bina/precache-manifest.4a052e82cecd3cb633b8e452bba12ad6.js"
 );
 
 workbox.clientsClaim();
